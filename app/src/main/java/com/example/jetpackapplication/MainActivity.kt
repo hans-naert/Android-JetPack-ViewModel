037a11df18e4024ccb498a817e9158badee8bd57
Hans Naert <hans.naert@vives.be>
2023-04-18 08:50:52 +0200

    add ViewModel

--- app/src/main/java/com/example/jetpackapplication/MainActivity.kt
+++ app/src/main/java/com/example/jetpackapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.jetpackapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.example.jetpackapplication.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        binding.plusOneButton.setOnClickListener {
+            viewModel.counter++
+            refreshCounter()
+        }
     }
+
+    fun refreshCounter(){
+        binding.infoTextView.text=viewModel.counter.toString()
+    }
+
 }
